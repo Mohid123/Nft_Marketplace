172c77b7ad22950e04c7d834eaa4360fe5ef2371
ijunaidkhan <junaidmehmood67@gmail.com>
2021-10-01 18:06:15 +0500

    nft card component(design implementation)

---
+++ src/app/@shell/ui/nft-card/nft-card.component.ts
@@ -0,0 +1,17 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+/* eslint-disable @angular-eslint/no-empty-lifecycle-method */
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nft-card',
+  templateUrl: './nft-card.component.html',
+  styleUrls: ['./nft-card.component.scss']
+})
+export class NftCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
