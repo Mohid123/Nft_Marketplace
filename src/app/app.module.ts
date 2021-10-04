946008542c21cd9171fa176fb2d50cc5f1517f35
mudassar2200 <mudassar.gulzar01@gmail.com>
2021-10-04 16:53:43 +0500

    Add Angular Meterial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { WebShellModule } from '@shell/ft/web-shell.module';
-// import { CoreModule } from './@core/core.module';
+import { CoreModule } from './@core/core.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, WebShellModule],
+  imports: [BrowserModule, WebShellModule, CoreModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
