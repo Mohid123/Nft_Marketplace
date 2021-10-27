55ddf889be98a04ead633208bedee2a9c9c6d310
Mohid <mohid.dastgeer@solissol.com>
2021-10-27 12:46:49 +0500

    Create NFT Ticket/Memebership

--- src/app/components/custom-dialogs/create-ntf-diloag/create-nft/create-nft.component.ts
+++ src/app/components/custom-dialogs/create-ntf-diloag/create-nft/create-nft.component.ts
@@ -2,14 +2,18 @@ import { ChangeDetectorRef, Component } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 import { Group } from '@app/@core/models/group.model';
 import { NFT } from '@app/@core/models/NFT.model';
+import { ResponseAddGroupMedia } from '@app/@core/models/response-add-media.model';
 import { ResponseGroupsByClub } from '@app/@core/models/response-groups-by-club.model';
+import { ApiResponse } from '@app/@core/models/response.model';
 import { CustomDialogService } from '@app/@core/services/custom-dialog/custom-dialog.service';
 import { GroupService } from '@app/@core/services/group.service';
 import { MediaService } from '@app/@core/services/media.service';
 import { NFTService } from '@app/@core/services/nft.service';
 import { RouteService } from '@app/@core/services/route.service';
-import { Subject } from 'rxjs';
-import { distinctUntilChanged, takeUntil } from 'rxjs/operators';
+import { AuthService } from '@app/pages/auth/services/auth.service';
+import * as htmlToImage from 'html-to-image';
+import { of, Subject } from 'rxjs';
+import { distinctUntilChanged, exhaustMap, take, takeUntil } from 'rxjs/operators';
 
 @Component({
   selector: 'app-create-nft',
@@ -20,17 +24,19 @@ export class CreateNFTComponent {
   group: Group
 
   public createNft: FormGroup;
-  type: any
-  email: any
-  date: any
-  selectedValue: any
-  public file: any
+  public imgFormData = new FormData();
+  type: any;
+  email: any;
+  date: any;
+  selectedValue: any;
+  public file: any;
   public format: string;
   public url: string;
 
 
   public clubName: string;
   public limit = 6 ;
+
 
   private _page:number;
   private _isLoading:boolean;
@@ -47,6 +53,7 @@ export class CreateNFTComponent {
     private nftService: NFTService,
     private groupService: GroupService,
     private routeService: RouteService,
+    private authService: AuthService
   ) {
     // this.createNft = this.formBuilder.group({
     //   email: new FormControl('', [Validators.required, Validators.email]),
@@ -57,6 +64,10 @@ export class CreateNFTComponent {
     // });
     this.createNft = this.formBuilder.group({
       file: new FormControl(''),
+      date: new FormControl(''),
+      address: new FormControl(''),
+      selectedValue: new FormControl(''),
+      type: this.nftService.createNft.type
     });
 
 
@@ -74,7 +85,7 @@ export class CreateNFTComponent {
     this.groupService.getAllGroupsByClub(this.clubName, this._page++, this.limit);
   }
 
-  onSelectFile(event) {
+  onSelectFile(event): void {
     this.file = event.target.files && event.target.files[0];
     console.log(this.file)
     if (this.file) {
@@ -110,6 +121,7 @@ export class CreateNFTComponent {
     this.nftService.createNft.serverCaptureFileUrl = this.file;
     this.nftService.createNft.createdAt = this.date;
     this.nftService.createNft.groupId = this.selectedValue;
+    this.nftService.createNft.type = this.type;
 
 
 
@@ -119,7 +131,7 @@ export class CreateNFTComponent {
   // });
 }
 
-addNft(){
+addNft() : void{
 
        // Create form data
        const formData = new FormData();
@@ -136,9 +148,70 @@ addNft(){
     this.customDialogService.closeDialogs();
   }
 
-  addImage(){
-    // this.nftService.uploadImage('Logo', this.file ).then((data)=>{
-    //       console.log('uploaded')
-    //    })
+  addImage() : void {
+    const node = document.getElementById('bg-image');
+    htmlToImage
+      .toPng(node, {
+        canvasWidth: 529,
+        canvasHeight: 487,
+        width: 529,
+        height: 487,
+        quality: 1,
+        pixelRatio: 1,
+        skipAutoScale: false,
+        // style: {
+        //   display: 'block',
+        //   transform: 'rotate(41.3deg)',
+        // }
+      })
+      .then((dataUrl) => {
+        this.createNft.patchValue({
+          file: this.mediaService.dataURLtoFile(
+            dataUrl,
+            this.createNft.controls.date.value + '.png',
+          ),
+        });
+        this.imgFormData.append('file', this.createNft.get('file').value);
+        this.mediaService.uploadMedia('group', this.imgFormData).pipe(take(1),
+            exhaustMap((res: ApiResponse<ResponseAddGroupMedia>) => {
+              if(!res.hasErrors()) {
+                const param: NFT = {
+                  type: this.nftService.createNft.type,
+                  createdAt: this.createNft.controls.date.value,
+                  groupId: this.createNft.controls.selectedValue.value,
+                  appPackageId: this.authService.loggedInUser?.appPackageId,
+                  serverCaptureFileUrl: res.data.url,
+                };
+                return this.nftService.addNft(param);
+              } else {
+                return of(null);
+              }
+            }),
+          ).subscribe((res:any) => {
+            if (res !== null) {
+              this.close();
+            } else {
+              alert('error :' + res.errors[0]?.error?.message);
+            }
+            console.log('res:', res);
+          });
+      })
+      .catch((error) => {
+        console.error('oops, something went wrong!', error);
+      });
+
       }
+
+  // Click on each image and display each individually on background div
+  showImageInDiv() : void {
+    const select = <HTMLImageElement>document.querySelector('#bg-image');
+    const tick = <HTMLImageElement>document.querySelector('#showImage');
+    Array.from(document.querySelectorAll('#ticket')).forEach(image => {
+      image.addEventListener('click', event => {
+        select.style.display = 'block';
+        tick.src = (event.target as HTMLImageElement).src;
+      })
+    })
+  }
+
 }
