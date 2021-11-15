b0c68cc6a3b980e4fdc011334ceead7fed35467c
mudassar2200 <mudassar.gulzar01@gmail.com>
2021-11-16 01:49:38 +0500

    trading history and admin sales page

--- src/app/pages/admin/admin-sale/admin-sale.page.ts
+++ src/app/pages/admin/admin-sale/admin-sale.page.ts
@@ -1,18 +1,96 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 /* eslint-disable @angular-eslint/no-empty-lifecycle-method */
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { NFTService } from '@app/@core/services/nft.service';
+import { Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import { Group } from './../../../@core/models/group.model';
+import { NFTList } from './../../../@core/models/NFTList.model';
+import { ApiResponse } from './../../../@core/models/response.model';
+import { CreatorService } from './../../../@core/services/creator.service';
+import { GroupService } from './../../../@core/services/group.service';
+import { RouteService } from './../../../@core/services/route.service';
+import { AuthService } from './../../auth/services/auth.service';
 
 @Component({
   selector: 'app-admin-sale.page',
   templateUrl: './admin-sale.page.html',
   styleUrls: ['./admin-sale.page.scss']
 })
-export class AdminSalePage implements OnInit {
+export class AdminSalePage implements OnInit, OnDestroy {
 
-  constructor() { }
+  creator$ = this.creatorService.Creator$;
+  destroy$ = new Subject();
 
-  ngOnInit(): void {
+  public clubName: string;
+
+  public nftList: NFTList;
+  public limit = 6 ;
+  public NftLimit = 12 ;
+  public page:number;
+
+  public groups$ = this.groupService.groups$;
+  public filterGroup: Group;
+
+  public isLoading:boolean;
+  public type = '';
+
+  constructor(
+    private authService: AuthService,
+    private creatorService: CreatorService,
+    private groupService: GroupService,
+    private nftService: NFTService,
+    private routeService: RouteService,
+  ) {
+    this.page = 1;
+    this.isLoading = false;
+    this.routeService.clubName$.pipe(takeUntil(this.destroy$)).subscribe((clubName) => {
+      this.clubName = clubName;
+      if(this.clubName)
+        this.getSoldNFTs();
+    });
   }
 
+  ngOnInit(): void {
+    this.groupService.getAllGroupsByClub(this.clubName, 0, 0);
+  }
 
+  getSoldNFTs():void {
+    this.nftService.getRecentSoldNfts(this.page, this.NftLimit ,this.filterGroup?.id, this.type).pipe(take(1))
+    .subscribe((result:ApiResponse<NFTList>) => {
+      console.log('result.dataresult.data:',result.data);
+      if (!result.hasErrors()) {
+        this.nftList = result.data;
+      }
+      this.isLoading = false;
+    });
+
+  }
+
+  setType(type:string): void {
+    this.type = type;
+    this.page = 1;
+    this.getSoldNFTs();
+  }
+
+  next():void {
+    this.page++;
+    this.getSoldNFTs();
+  }
+
+  previous():void {
+    this.page--;
+    this.getSoldNFTs();
+  }
+
+  filterBy (group:Group) :void {
+    this.page = 1;
+    this.filterGroup = group;
+    this.getSoldNFTs();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
+  }
 }
--- src/app/pages/home/trading-history/trading-history.page.ts
+++ src/app/pages/home/trading-history/trading-history.page.ts
@@ -1,15 +1,86 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Group } from '@app/@core/models/group.model';
+import { GroupService } from '@app/@core/services/group.service';
+import { NFTService } from '@app/@core/services/nft.service';
+import { RouteService } from '@app/@core/services/route.service';
+import { AuthService } from '@app/pages/auth/services/auth.service';
+import { Subject } from 'rxjs';
+import { take, takeUntil } from 'rxjs/operators';
+import { ResponseEventByNFT } from './../../../@core/models/response-events-by-nft.model';
+import { ApiResponse } from './../../../@core/models/response.model';
 
 @Component({
   selector: 'app-trading-history',
   templateUrl: './trading-history.page.html',
   styleUrls: ['./trading-history.page.scss']
 })
-export class TradingHistoryPage implements OnInit {
+export class TradingHistoryPage implements OnInit, OnDestroy {
 
-  constructor() { }
+  destroy$ = new Subject();
 
-  ngOnInit(): void {
+  public clubName: string;
+  public responseEventByNFT: ResponseEventByNFT;
+  public limit = 6 ;
+  public eventLimit = 12 ;
+  public page:number;
+
+  public groups$ = this.groupService.groups$;
+  public filterGroup: Group;
+
+  public isLoading:boolean;
+  public type = '';
+
+  constructor(
+    private authService: AuthService,
+    private groupService: GroupService,
+    private nftService: NFTService,
+    private routeService: RouteService,
+  ) {
+    this.page = 1;
+    this.isLoading = false;
+    this.routeService.clubName$.pipe(takeUntil(this.destroy$)).subscribe((clubName) => {
+      this.clubName = clubName;
+      if(this.clubName)
+        this.getEvetns();
+    });
   }
 
+  ngOnInit(): void {
+    this.groupService.getAllGroupsByClub(this.clubName, 0, 0);
+  }
+
+  getEvetns():void {
+    this.nftService.getEventsByUser(this.authService.loggedInUser.id,this.page, this.eventLimit ,this.filterGroup?.id, this.type).pipe(take(1)).subscribe((result:ApiResponse<ResponseEventByNFT>) => {
+      if (!result.hasErrors()) {
+        this.responseEventByNFT = result.data;
+      }
+    });
+  }
+
+  setType(type:string): void {
+    this.type = type;
+    this.page = 1;
+    this.getEvetns();
+  }
+
+  next():void {
+    this.page++;
+    this.getEvetns();
+  }
+
+  previous():void {
+    this.page--;
+    this.getEvetns();
+  }
+
+  filterBy (group:Group) :void {
+    this.page = 1;
+    this.filterGroup = group;
+    this.getEvetns();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.complete();
+    this.destroy$.unsubscribe();
+  }
 }
