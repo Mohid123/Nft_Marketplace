0124baf682c51e3d501a0ca09f14efdd7c30a5de
mudassar2200 <mudassar.gulzar01@gmail.com>
2021-11-25 16:11:49 +0500

    route issue fix

--- src/app/@core/guards/no-auth.guard.ts
+++ src/app/@core/guards/no-auth.guard.ts
@@ -1,23 +1,32 @@
 import { Injectable } from '@angular/core';
-import { CanLoad, Router } from '@angular/router';
+import { ActivatedRouteSnapshot, CanActivate, Router, UrlTree } from '@angular/router';
+import { getItem, StorageItem } from '@app/@core/utils';
 import { AuthService } from '@app/pages/auth/services/auth.service';
+import { Observable } from 'rxjs';
 import { ROUTER_UTILS } from '../utils/router.utils';
 import { RouteService } from './../services/route.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class NoAuthGuard implements CanLoad {
+export class NoAuthGuard implements CanActivate {
   constructor(
     private authService: AuthService,
     private router: Router,
     private routeService: RouteService,
   ) {}
 
-  canLoad(): boolean {
+  canActivate(route: ActivatedRouteSnapshot):
+  | Observable<boolean | UrlTree>
+  | Promise<boolean | UrlTree>
+  | boolean
+  | UrlTree {
     const isLoggedIn = this.authService.isLoggedIn;
 
     if (isLoggedIn) {
+      if(route?.params?.clubName !== getItem(StorageItem.ActiveClub)) {
+        return true;
+      }
       this.router.navigate([
         this.routeService.clubName || '/',
         ROUTER_UTILS.config.base.home,
