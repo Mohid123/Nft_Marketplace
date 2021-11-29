ccad08c4681b8e7768b269cef23a37e378b665c1
ijunaidkhan <junaid.mehmood@solissol.com>
2021-11-29 10:34:35 +0500

    internet connectivitiy issue

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
+import { ViewportScroller } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { ROLE_TYPE_UTILS } from '@app/@core/utils/role-type.utils';
 import { AuthService } from '@app/pages/auth/services/auth.service';
 import { SeoService } from '@core/services/seo';
 import { ThemeService } from '@core/services/theme';
-import { Observable } from 'rxjs';
+import { ToastrService } from 'ngx-toastr';
+import { fromEvent, Observable, Subscription } from 'rxjs';
 import { CreatorService } from './@core/services/creator.service';
 import { CustomDialogService } from './@core/services/custom-dialog/custom-dialog.service';
 import { RouteService } from './@core/services/route.service';
@@ -15,12 +17,18 @@ import { RouteService } from './@core/services/route.service';
 })
 export class AppComponent implements OnInit {
 
+  onlineEvent: Observable<Event>;
+  offlineEvent: Observable<Event>;
+  subscriptions: Subscription[] = [];
+
   isLoggedIn$: Observable<boolean> = this.authService.isLoggedIn$;
   role$: Observable<ROLE_TYPE_UTILS> = this.authService.role$;
   isAdminPanel$: Observable<boolean> = this.routeService.isAdminPanel$;
   creator$ = this.creatorService.Creator$;
 
   roles = ROLE_TYPE_UTILS;
+
+
 
   constructor(
     private seoService: SeoService,
@@ -29,16 +37,50 @@ export class AppComponent implements OnInit {
     private creatorService: CreatorService,
     private customDialogService: CustomDialogService,
     private routeService: RouteService,
+    private viewportScroller: ViewportScroller,
+    private toastr: ToastrService,
   ) {
     this.routeService.listenToRouter();
   }
 
   ngOnInit(): void {
     this.runGlobalServices();
+    // OFFLINE/ONLINE CHECK
+    this.onlineEvent = fromEvent(window, "online");
+    this.offlineEvent = fromEvent(window, "offline");
+    //ONLINE CHECK
+    this.subscriptions.push(
+      this.onlineEvent.subscribe((e) => {
+        this.toastr.success("You are now back online", "Internet Status");
+      })
+    );
+    //OFFLINE CHECK
+    this.subscriptions.push(
+      this.offlineEvent.subscribe((e) => {
+        this.toastr.error(
+          "You are offline. Please Check your Internet Connection",
+          "Internet Status"
+        );
+      })
+    );
   }
+
+  ngOnDestroy() {
+    // this.unsubscribe.forEach((sb) => sb.unsubscribe());
+    // UNSUBSCRIBE FROM OBSERVABLE
+    this.subscriptions.forEach((subscription) => subscription.unsubscribe());
+  }
+
+
 
   private runGlobalServices(): void {
     this.seoService.init();
     this.themeService.init();
+  }
+
+  onActivate(event):void {
+    if(document.getElementById('admin-main')) {
+      document.getElementById('admin-main').scrollIntoView()
+    }
   }
 }
