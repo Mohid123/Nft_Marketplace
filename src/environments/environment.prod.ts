46e623c5b44f1b8a8e3a5668ebf48008d5902bda
ijunaidkhan <junaid.mehmood@solissol.com>
2022-05-30 16:22:52 +0500

    testnet prod file url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -11,14 +11,14 @@ export const environment = {
   demoClubEmail1: 'admin@gmail.com',
   demoClubPassword1: 'qwertyuiop',
 
-  // firebase: {
-  //   apiKey: "AIzaSyCHyGMm-OaTigJU1l3ynVH8L0enkl34xPI",
-  // authDomain: "nft-auth-app.firebaseapp.com",
-  // projectId: "nft-auth-app",
-  // storageBucket: "nft-auth-app.appspot.com",
-  // messagingSenderId: "242147439537",
-  // appId: "1:242147439537:web:9e9a6a2b494a05ce7ec390",
-  // measurementId: "G-WD23R4NTBM"
-  // }
+  firebase: {
+    apiKey: "AIzaSyCHyGMm-OaTigJU1l3ynVH8L0enkl34xPI",
+  authDomain: "nft-auth-app.firebaseapp.com",
+  projectId: "nft-auth-app",
+  storageBucket: "nft-auth-app.appspot.com",
+  messagingSenderId: "242147439537",
+  appId: "1:242147439537:web:9e9a6a2b494a05ce7ec390",
+  measurementId: "G-WD23R4NTBM"
+  }
 
 };
